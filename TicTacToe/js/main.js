de552b970e02d65a8b9249c6858373a373fcb20f
Maks <maksveremey4ik@gmail.com>
2021-02-11 16:04:00 +0200

    push game

---
+++ TicTacToe/js/main.js
@@ -0,0 +1,97 @@
+const game = document.getElementById("game");
+const block = document.getElementsByClassName("block");
+const batman = document.getElementById("batman");
+const groot = document.getElementById("groot");
+const buttonRestart = document.getElementById("button-rest");
+
+let hod = 0;
+
+const fn = (event) => {
+  const batAudio = new Audio("audio/batman.mp3");
+  const groAudio = new Audio("audio/groot.mp3");
+  const blockItem = event.target;
+
+  if (event.target.id !== "game") {
+    hod++;
+    hod % 2 == 0
+      ? ((blockItem.className = "batman block"), batAudio.play())
+      : ((blockItem.className = "groot block"), groAudio.play());
+    checkWinner();
+  }
+};
+
+function checkWinner() {
+  let combArr = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < combArr.length; i++) {
+    if (
+      block[combArr[i][0]].className == "batman block" &&
+      block[combArr[i][1]].className == "batman block" &&
+      block[combArr[i][2]].className == "batman block"
+    ) {
+      batman.innerText++;
+      setTimeout(() => {
+        block[combArr[i][0]].classList.add("winnerComb", "batman-bg");
+        block[combArr[i][1]].classList.add("winnerComb", "batman-bg");
+        block[combArr[i][2]].classList.add("winnerComb", "batman-bg");
+      }, 100);
+      for (const item of block) {
+        item.removeEventListener("click", fn, {
+          once: true,
+        });
+      }
+    } else if (
+      block[combArr[i][0]].className == "groot block" &&
+      block[combArr[i][1]].className == "groot block" &&
+      block[combArr[i][2]].className == "groot block"
+    ) {
+      groot.innerText++;
+      setTimeout(() => {
+        block[combArr[i][0]].classList.add("winnerComb", "groot-bg");
+        block[combArr[i][1]].classList.add("winnerComb", "groot-bg");
+        block[combArr[i][2]].classList.add("winnerComb", "groot-bg");
+      }, 100);
+      for (const item of block) {
+        item.removeEventListener("click", fn, {
+          once: true,
+        });
+      }
+    } else if (hod == 9) {
+      block[combArr[i][0]].classList.add("winnerComb", "draw-bg");
+      block[combArr[i][1]].classList.add("winnerComb", "draw-bg");
+      block[combArr[i][2]].classList.add("winnerComb", "draw-bg");
+      for (const item of block) {
+        item.removeEventListener("click", fn, {
+          once: true,
+        });
+      }
+    }
+  }
+}
+
+function restarFunc() {
+  hod = 0;
+  for (const item of block) {
+    item.className = "block";
+    item.addEventListener("click", fn, {
+      once: true,
+    });
+  }
+}
+
+for (const item of block) {
+  item.addEventListener("click", fn, {
+    once: true,
+  });
+}
+
+buttonRestart.addEventListener("click", restarFunc);
